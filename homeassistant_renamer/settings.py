a5ff8b3f62ae6450e7341437ef5553d7b49ec9bd
Coenenp <pieter.coenen@gmail.com>
2024-05-20 17:00:47 +0700

    Update settings.py

--- homeassistant_renamer/settings.py
+++ homeassistant_renamer/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-xue@mkkfj(^b9c-%+swq+t693_qm(l5+bey#y#74j+f69xq=6m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.vercel.app',

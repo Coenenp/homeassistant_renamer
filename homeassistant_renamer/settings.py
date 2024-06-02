06dde0dcaab356d42b99571b8ae03af2aa6ae668
Coenenp <pieter.coenen@gmail.com>
2024-06-02 20:10:38 +0700

    Update settings.py

--- homeassistant_renamer/settings.py
+++ homeassistant_renamer/settings.py
@@ -91,10 +91,10 @@ WSGI_APPLICATION = 'homeassistant_renamer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'POSTGRES_DATABASE', 
-        'USER': 'POSTGRES_USER',
-        'PASSWORD': 'POSTGRES_PASSWORD',
-        'HOST': 'POSTGRES_HOST', 
+        'NAME':  config('POSTGRES_DATABASE'), 
+        'USER':  config('POSTGRES_USER'),
+        'PASSWORD':  config('POSTGRES_PASSWORD'),
+        'HOST':  config('POSTGRES_HOST'), 
         'PORT': '5432',
     }
 }

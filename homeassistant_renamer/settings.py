69e41b6d51ae474d47f735800702621edab783cc
Coenenp <pieter.coenen@gmail.com>
2024-06-02 20:01:20 +0700

    Change to postgres for vercel deployment

--- homeassistant_renamer/settings.py
+++ homeassistant_renamer/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'homeassistant_renamer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'POSTGRES_DATABASE', 
+        'USER': 'POSTGRES_USER',
+        'PASSWORD': 'POSTGRES_PASSWORD',
+        'HOST': 'POSTGRES_HOST', 
+        'PORT': '5432',
     }
 }
 
